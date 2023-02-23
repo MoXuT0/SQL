b455fff3f18ccd05a7037b9dbe5bc0b220caca5b
MoXuT0 <DenisFisenko2010@yandex.ru>
2023-02-23 11:36:53 +0300

    Реализация методов

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -17,14 +17,28 @@ public class Application {
         EmployeeDao employeeDao = new EmployeeDaoImpl();
         CityDao cityDao = new CityDaoImpl();
 
-        City samara = new City("Samara");
+        City city = new City("Vladivostok");
 
-        List<Employee> employees = List.of(new Employee("Anton", "Antonov", "male", 32, samara),
-                new Employee("Petr", "Petrov", "male", 39, samara));
+        List<Employee> employees = List.of(new Employee("Anton", "Antonov", "male", 32, city),
+                new Employee("Petr", "Petrov", "male", 39, city));
 
-        samara.setEmployees(employees);
+        city.setEmployees(employees);
 
-        cityDao.add(samara);
+        cityDao.add(city);
+        employeeDao.getAll().forEach(System.out::println);
+
+        cityDao.getById(city.getCityId());
+
+        cityDao.getAll().forEach(System.out::println);
+
+        city.setCityName("Volgograd");
+        cityDao.update(city);
+        cityDao.getAll().forEach(System.out::println);
+
+        cityDao.delete(city);
+
+        employeeDao.getAll().forEach(System.out::println);
+        cityDao.getAll().forEach(System.out::println);
 
     }
 
